ae079293a7d1b476a33874c04be48da0fe8ec576
Shane Murphy <shn.murphy@gmail.com>
2024-03-04 15:42:17 +0000

    Change paragraph Text storage option tablet

--- app/compontents/Storage-options-swiper.jsx
+++ app/compontents/Storage-options-swiper.jsx
@@ -32,14 +32,12 @@ const containerData = [
 export default function StorageOptionsSwiper() {
   return (
     <div>
-      <div className="laotop:w-10/12 laptop:my-[30%] md:my-[40%] sm:my-[50%] xl:hidden lg:hidden laptop:visible md:visible sm:visible">
+      <div className="laotop:w-10/12 laptop:my-[30%] md:my-[30%] sm:my-[60%] xl:hidden lg:hidden laptop:visible md:visible sm:visible">
         <h3 className="laptop:text-6xl md:text-5xl sm:text-3xl  text-myblue text-center font-bold mb-1">
           Storage Containers
         </h3>
-        <p className="laptop:w-9/12 md:w-10/12 sm:w-11/12 mx-auto text-center font-medium laptop:text-xl md:text-lg sm:text-xs text-myblack">
-          We offer a wide range of container types, sizes. Whether you require
-          short-term or long-term storage, we have the flexibility to
-          accommodate your requirements
+        <p className="laptop:w-9/12 md:w-10/12 sm:w-10/12 mx-auto text-center font-medium laptop:text-xl md:text-lg sm:text-base text-myblack">
+          We offer a wide range of container sizes 10FT, 20FT and 40FT.
         </p>
       </div>
     </div>
