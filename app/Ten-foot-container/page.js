5fcbe87c931ded5dd11b114b466ed3c7a0a428c7
Shane Murphy <shn.murphy@gmail.com>
2024-03-14 11:18:28 +0000

    Updates to 10ft container page

--- app/Ten-foot-container/page.js
+++ app/Ten-foot-container/page.js
@@ -2,7 +2,7 @@ export default function TenFootContainerPage() {
   return (
     <div>
       <div className="w-full bg-myblue lg:h-[80vh] md:h-[85vh] sm:h-svh lg:py-14 lg:px-14 antialiased">
-        <div className="xl:w-10/12 lg:w-11/12 md: laptop:w-11/12 sm:w-11/12 mx-auto lg:py-[6%] md:py-[15%] sm:py-[32%]">
+        <div className="xl:w-10/12 lg:w-11/12 md: laptop:w-11/12 sm:w-11/12 mx-auto lg:py-[6%] md:py-[15%] sm:py-[25%]">
           <div className="flex flex-row xl:gap-32 lg:gap-10">
             <div className="lg:basis-1/3">
               <div className="bg-mywhite xl:w-[300px] lg:w-[280px] py-7 px-7 lg:mt-5 rounded-xl xl:block lg:block md:hidden sm:hidden xs:hidden">
@@ -57,8 +57,8 @@ export default function TenFootContainerPage() {
               </div>
             </div>
           </div>
-          <div className="lg:hidden md:block md:w-10/12 sm:w-11/12 mx-auto bg-mywhite md:py-6 md:px-2 sm:py-4 sm:px-2 mt-6 rounded-3xl">
-            <div className="grid md:grid-cols-2 md:divide-x md:divide-y-0 sm:divide-y sm:px-5 gap-8">
+          <div className="lg:hidden md:block md:w-10/12 sm:w-11/12 mx-auto bg-mywhite md:py-6 md:px-2 sm:py-4 sm:px-2 sm:mt-4 rounded-3xl">
+            <div className="grid md:grid-cols-2 md:divide-x md:divide-y-0 sm:divide-y sm:px-5 md:gap-8 sm:gap-2">
               <div>
                 <h6 className="text-lg font-semibold text-myblue">
                   Storage Capacity
@@ -68,7 +68,14 @@ export default function TenFootContainerPage() {
                   space
                 </p>
               </div>
-              <div>BBBB</div>
+              <div>
+                <h6 className="text-lg md:pl-6 sm:mt-1 font-semibold text-myblue">
+                  Dimensions
+                </h6>
+                <p className="text-myblack md:pl-6 font-medium text-xs">
+                  10ft long, 7ft wide, and 7ft tall
+                </p>
+              </div>
             </div>
           </div>
         </div>
